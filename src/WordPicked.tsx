3425b1c74fefa52923eb8b5712fa09e9d40fb82a
Rita Alves <88343071+ritaalves95@users.noreply.github.com>
2022-04-20 13:34:28 +0100

    add confettis

--- src/WordPicked.tsx
+++ src/WordPicked.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useCallback, useRef, useEffect } from 'react'
+import ReactCanvasConfetti from "react-canvas-confetti";
 
 type Props = {
     word: string;
@@ -7,6 +8,56 @@ type Props = {
 }
 
 const WordPicked = ({word, resetTags, resetWord}: Props) => {
+//  confetti
+const refAnimationInstance: any = useRef(null);
+
+const getInstance = useCallback((instance: any) => {
+  refAnimationInstance.current = instance;
+}, []);
+
+const makeShot = useCallback((particleRatio: any, opts:any) => {
+  refAnimationInstance.current &&
+    refAnimationInstance.current({
+      ...opts,
+      origin: { y: 0.7 },
+      particleCount: Math.floor(200 * particleRatio)
+    });
+}, []);
+
+const fire = useCallback(() => {
+  makeShot(0.25, {
+    spread: 26,
+    startVelocity: 55
+  });
+
+  makeShot(0.2, {
+    spread: 60
+  });
+
+  makeShot(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8
+  });
+
+  makeShot(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2
+  });
+
+  makeShot(0.1, {
+    spread: 120,
+    startVelocity: 45
+  });
+}, [makeShot]);
+
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => {fire()}, [])
+
+//  restart     
     const restart = () => {
         resetTags();
         resetWord();
@@ -18,6 +69,8 @@ const WordPicked = ({word, resetTags, resetWord}: Props) => {
             <h1 className='word-picked'>{word}</h1>
             <button onClick={restart}>Thank you</button>
         </article>
+
+        <ReactCanvasConfetti refConfetti={getInstance} className="confetti-wrapper" />
     </div>
   )
 }
