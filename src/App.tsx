9f6624572ca9d2ffafdf8fbbc80fee45c90833a2
Rita Alves <88343071+ritaalves95@users.noreply.github.com>
2022-04-20 12:14:40 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+//  handle key up event
+  const handleKeyUp = (e: React.KeyboardEvent):void => {
+    console.log((e.target as HTMLInputElement).value)
+    let value = (e.target as HTMLInputElement).value
+
+    createTags(value);
+
+    if(e.key === 'Enter') {
+      setTimeout(()=> {
+          // e.target.value = ''
+      }, 10)
+      randomSelect()
+    }
+  }
+
+//  create tags
+  const [tags, setTags] = useState<string[]>([]);
+
+  const createTags = (input: string): void =>{
+    const tagsValue = input.split('.').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+    setTags(tagsValue)
+  }
+
+//  random selection
+  const randomSelect = () => {
+    const times = 30;
+
+    const interval = setInterval(()=>{
+        const randomTag = pickRandomTag()
+        highlightTag(randomTag);
+        
+        setTimeout(()=>{
+            removeHighlightTag(randomTag)
+        }, 100)
+    }, 100);
+
+    setTimeout(()=>{
+        clearInterval(interval);
+
+        setTimeout(()=>{
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag)
+        }, 100)
+    },times * 100)
+  }
+
+  const pickRandomTag = (): Element => {
+    const allTags = document.getElementsByClassName('tag')
+    return allTags[Math.floor(Math.random() * tags.length)]
+  }
+
+  const highlightTag = (tag: Element): void => {
+    tag.classList.add('highlight');
+  }
+
+  const removeHighlightTag = (tag: Element): void => {
+      tag.classList.remove('highlight');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3>Enter all of the choices divided by a comma (' . '). <br />
+          Press enter when you're done</h3>
+
+      <textarea id="textarea" placeholder="Enter choices here..." onKeyUp={handleKeyUp} autoFocus></textarea>
+
+      <div id="tags">
+        {tags.map((tagEl, index) => {
+          return(
+            <span className='tag' key={index}>{tagEl}</span>
+          )
+        })}
+      </div>
     </div>
   );
 }
